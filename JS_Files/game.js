36a93ebaf7570970efac89531b5018a45c3de942
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-06-11 20:50:54 +0530

    Physics,Gravity&Collisions

--- JS_Files/game.js
+++ JS_Files/game.js
@@ -8,17 +8,29 @@ let config={
     },
     backgroundColor:0xfff111,
     
+    physics:{
+        default:'arcade',
+        arcade:{
+            gravity:{
+                y:1000,
+            }
+        }
+    },
+    
     scene:{
         preload:preload,
         create:create,
         update:update,
-    }
+    },
 };
 let game=new Phaser.Game(config);
 
 function preload(){
     this.load.image("ground","./Assets/topground.png");
     this.load.image("sky","./Assets/background.png");
+    this.load.spritesheet('dude',"./Assets/dude.png",{frameWidth:32,frameHeight:48});
+    
+    
 }
 
 function create(){
@@ -35,6 +47,21 @@ function create(){
     background.depth=-1;
     background.displayWidth=W;
     background.displayHeight=H;
+    
+    //Adding player
+    let player=this.physics.add.sprite(100,100,'dude',4);
+    
+    this.physics.add.existing(ground);
+    ground.body.allowGravity=false;
+    ground.body.immovable=true;
+    //if we directly make ground as static by writing 
+    //this.physics.add.existing(ground,true);true->make it static(false by default)
+    //no need to write these
+    // ground.body.allowGravity=false;
+    //ground.body.immovable=true;
+    
+    //add a collision detection between player and ground
+    this.physics.add.collider(player,ground);
     
     
 }
